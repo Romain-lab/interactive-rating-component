e951c9236f5eae3c8a8f23b924409febd126a1dc
Romain <evanxprod@gmail.com>
2022-07-20 10:17:36 +0200

    added challenge files + upgraded CRA

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*Rating state start*/}
+
+      How did we do?
+
+      Please let us know how we did with your support request. All feedback is appreciated
+      to help us improve our offering!
+
+      1 2 3 4 5
+
+      Submit
+
+      {/*Rating state end*/}
+
+      {/*Thank you state start*/}
+
+      You selected {/*Add rating here*/} out of 5
+
+      Thank you!
+
+      We appreciate you taking the time to give a rating. If you ever need more support,
+      don’t hesitate to get in touch!
+
+      {/*Thank you state end*/}
+
+
+      <div class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+        Coded by <a href="#">Your Name Here</a>.
+      </div>
     </div>
   );
 }
